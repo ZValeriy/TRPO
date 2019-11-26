d441d737648e75bfdb472c50548e3816a32a3ba0
Valeriy <valeriyzubairov@gmail.com>
2019-11-26 18:21:31 +0300

    add functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  isStatementHidden = true;
+  isStatementRejectReasonHidden = true;
+  isStatementSubmittedHidden = true;
+  isFindStatementHidden = true;
+  isLoginFormHidden = true;
+  get isMaskHidden() {
+    return !(!this.isStatementSubmittedHidden || !this.isLoginFormHidden ||
+      !this.isFindStatementHidden || !this.isStatementHidden || !this.isStatementRejectReasonHidden);
+  };
+
+  openLoginForm() {
+    this.isLoginFormHidden = false;
+  }
+
+  hidePopupsAndMasks() {
+    this.isLoginFormHidden = true;
+    this.isStatementHidden = true;
+    this.isFindStatementHidden = true;
+    this.isStatementRejectReasonHidden = true;
+    this.isStatementSubmittedHidden = true;
+  }
 }
